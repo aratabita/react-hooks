3e21168ab34616b6a91b2abcb012c970bd95df2a
For Private <>
2020-11-03 13:07:50 +0900

    add deletion log

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { DELETE_EVENT } from '../actions';
+import { ADD_OPERATION_LOG, DELETE_EVENT } from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../util';
 
 // @ts-ignore
 const Event = ({ event }) => {
@@ -14,6 +15,11 @@ const Event = ({ event }) => {
     dispatch({
       type: DELETE_EVENT,
       id,
+    });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: `${id}のイベントを削除しました。`,
+      operatedAt: timeCurrentIso8601(),
     });
   };
   return (
